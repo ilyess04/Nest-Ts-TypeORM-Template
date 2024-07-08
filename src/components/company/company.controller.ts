fb0c3bc54430ce40597124211bf70fe94aa72c11
Sellamingo <ilyessellami04@gmail.com>
2024-07-08 16:08:13 +0100

    Configure Request Header

--- src/components/company/company.controller.ts
+++ src/components/company/company.controller.ts
@@ -6,39 +6,48 @@ import {
   Post,
   Res,
   UseGuards,
-} from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { CompanyService } from './company.service';
-import { Response } from 'express';
-import { CreateCompanyDto } from './dto';
-import { AuthGuard } from '@nestjs/passport';
-import { JwtStrategy } from 'src/common/strategy/jwt.srategy';
-import { openApiResponse } from 'src/common/decorator/openApi.decorator';
+  Request,
+} from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { CompanyService } from "./company.service";
+import { Response } from "express";
+import { CreateCompanyDto } from "./dto";
+import { AuthGuard } from "@nestjs/passport";
+import { JwtStrategy } from "src/common/strategy/jwt.srategy";
+import { openApiResponse } from "src/common/decorator/openApi.decorator";
+import { IRequest } from "src/common/interfaces";
 
-@ApiTags('company')
-@Controller('company')
+@ApiTags("company")
+@Controller("company")
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
-  @UseGuards(AuthGuard('jwt'), JwtStrategy)
-  @Post('create')
+  @UseGuards(AuthGuard("jwt"), JwtStrategy)
+  @Post("create")
   @openApiResponse(
     {
       status: HttpStatus.CREATED,
-      description: 'Company created successfuly !',
+      description: "Company created successfuly !",
     },
     {
       status: HttpStatus.INTERNAL_SERVER_ERROR,
-      description: 'something went wrong!',
-    },
+      description: "something went wrong!",
+    }
   )
-  async CreateCompany(@Res() res: Response, @Body() body: CreateCompanyDto) {
+  async createCompany(
+    @Res() res: Response,
+    @Body() body: CreateCompanyDto,
+    @Request() req: IRequest
+  ) {
     try {
-      const company = await this.companyService.createCompany(body);
+      const company = await this.companyService.createCompany({
+        managerId: req.user.id,
+        ...body,
+      });
       return res.status(HttpStatus.CREATED).send({
         statusCode: HttpStatus.CREATED,
         company,
-        message: 'Company created successfuly !',
+        message: "Company created successfuly !",
       });
     } catch (err) {
       throw new InternalServerErrorException({
--- src/components/company/dto/index.ts
+++ src/components/company/dto/index.ts
@@ -1,11 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString } from "class-validator";
+import { IsString } from "class-validator";
 
 export class CreateCompanyDto {
   @IsString()
   @ApiProperty()
   name: string;
-  @IsNumber()
-  @ApiProperty()
-  managerId: number;
 }
